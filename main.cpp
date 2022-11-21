b2e4a5ceb34405871d51554e1e96b79f79d5a584
jacopomazzo <114420544+jacopomazzo@users.noreply.github.com>
2022-11-21 18:37:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+   int i, n = 0, a=1, b=1, c;
+   cin>>n;
+
+   if (n>=2){
+      cout<<a<<endl<<b<<endl;
+      for(i=2;i<n;i++){
+          c=a+b;
+          a=b;
+          b=c;
+          cout<<c<<endl;
+     }else{
+       cout<<"errore"<<endl;
+  }
+      return 0;
 }
